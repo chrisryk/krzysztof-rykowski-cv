0fd714fab5f735e035b686ca5cf2917f72df108c
Krzysztof Rykowski <rykowski.krzysztof@gmail.com>
2022-04-22 12:40:09 +0200

    feat: add scroll arrow

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const sections = [...document.querySelectorAll('section')];
+const arrowDiv = document.querySelector('.arrow');
+const arrowImg = document.querySelector('.arrow img');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
     const scrollValue = this.scrollY;
     const viewportHeight = window.innerHeight;
+
+    if (scrollValue > 0) {
+        arrowDiv.classList.add('active');
+        arrowImg.classList.add('roll');
+    }
+    else {
+        arrowDiv.classList.remove('active');
+        arrowImg.classList.remove('roll');
+    }
     
     sections.forEach(section => {
         const sectionPosition = section.offsetTop;
